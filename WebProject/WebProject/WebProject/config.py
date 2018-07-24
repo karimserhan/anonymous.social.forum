5c3813811eaf629a531ec5d0f2f1eb9bd73555a5
Karim Serhan <kaserhan@microsoft.com>
2018-07-24 00:00:35 -0700

    v1 user schema

--- WebProject/WebProject/WebProject/config.py
+++ WebProject/WebProject/WebProject/config.py
@@ -3,9 +3,9 @@
 import os
 import urllib
 
-params = urllib.parse.quote_plus("DRIVER={SQL Server};SERVER=anonymatch.database.windows.net;DATABASE=anonymatchdb;UID=anonymatchuser;PWD=@nonyMatch")
+db_params = urllib.parse.quote_plus('Driver={SQL Server};Server=tcp:anonymatch.database.windows.net,1433;Database=anonymatchdb;uid=anonymatchuser;pwd=@nonyMatch')
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc:///?odbc_connect=%s' % params
+    SQLALCHEMY_DATABASE_URI='mssql+pyodbc:///?odbc_connect={}'.format(db_params)
     SQLALCHEMY_COMMIT_ON_TEARDOWN = False
